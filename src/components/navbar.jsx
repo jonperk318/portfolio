68ebe0a3525890f712796c6982fadb7bcac9ec03
Jonathan Perkins <jperkin4@uab.edu>
2024-08-20 21:48:09 -0500

    Creating navbar and links

---
+++ src/components/navbar.jsx
@@ -0,0 +1,84 @@
+"use client"
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+import { IconContext } from "react-icons";
+import { FaFacebook, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import Navlink from "./navlink";
+
+const links = [
+    {url: "/", title: "Home"},
+    {url: "/about", title: "About"},
+    {url: "/projects", title: "Projects"},
+    {url: "/contact", title: "Contact"},
+];
+
+const Navbar = () => {
+
+    const [open, setOpen] = useState(false)
+
+    return (
+        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-30">
+            {/* LOGO */}
+            <div className="z-40 justify-center">
+                <Link href="/">
+                    <Image src="/public/logo.png" className="w-16 hover:scale-125"/>
+                </Link>
+            </div>
+            {/* LINKS */}
+            <div className="hidden md:flex gap-8">
+                {links.map(link => (
+                    <Navlink link={link} key={link.title} />
+                ))}
+            </div>
+            {/* ICONS */}
+            <div className="hidden md:flex gap-4">
+                <IconContext.Provider value={{className: "hover:scale-125 hover:fill-watermelon", size:24}}>
+                    <a href="https://github.com/jonperk318">
+                        <FaGithub />
+                    </a>
+                </IconContext.Provider>
+                <IconContext.Provider value={{className: "hover:scale-125 hover:fill-yellow", size:24}}>
+                    <a href="https://www.instagram.com/jonperk318/">
+                        <FaInstagram />
+                    </a>
+                </IconContext.Provider>
+                <IconContext.Provider value={{className: "hover:scale-125 hover:fill-light-blue", size:24}}>
+                    <a href="https://www.linkedin.com/in/jonathan-perkins-0b1411285/">
+                        <FaLinkedin />
+                    </a>
+                </IconContext.Provider>
+                <IconContext.Provider value={{className: "hover:scale-125 hover:fill-blue", size:24}}>
+                    <a href="https://www.facebook.com/jonathan.davidperkins">
+                        <FaFacebook />
+                    </a>
+                </IconContext.Provider>
+                <IconContext.Provider value={{className: "hover:scale-125 hover:fill-lilac", size:24}}>
+                    <a href="mailto:jonperk318@gmail.com">
+                        <IoMdMail />
+                    </a>
+                </IconContext.Provider>
+            </div>
+            {/* MENU */}
+            <div className="md:hidden">
+                {/* HAMBURGER */}
+                <button className="w-10 h-8 flex flex-col justify-between z-50 relative" onClick={()=>setOpen(!open)}>
+                    <div className="w-10 h-1 bg-fusha rounded"></div>
+                    <div className="w-10 h-1 bg-fusha rounded"></div>
+                    <div className="w-10 h-1 bg-fusha rounded"></div>
+                </button>
+                {/* MENU LINKS */}
+                {open && (
+                    <div className="absolute top-0 left-0 w-screen h-screen animated-background bg-black bg-gradient-to-r from-black from-10% via-watermelon/[0.3] to-black flex flex-col items-center justify-center gap-10 text-4xl">
+                        {links.map(link => (
+                            <Link href={link.url} key={link.title}>{link.title}</Link>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      "black": "#0f1416",
+      "space-cadet": "#282d40",
+      "davys-gray": "#4a495a",
+      "taupe": "#83828b",
+      "silver": "#bcbcbc",
+      "salmon": "#fe433c",
+      "watermelon": "#f31d64",
+      "lilac": "#a224ad",
+      "fusha": "#db1df3",
+      "purple": "#6a38b3",
+      "lavender": "#bdadff",
+      "blue": "#3c5081",
+      "light-blue": "#0095ef",
+      "yellow": "#efca00",
+      "teal": "#43c68b"
+    }
+  },
+  plugins: [],
+};
